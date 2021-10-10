26398d98d7a9a17077450d3a036ec340928ca827
WarriorX623 <owenhwtam@gmail.com>
2021-10-10 15:16:56 +0800

    Sticky nav

--- script.js
+++ script.js
@@ -95,3 +95,21 @@ sliderLeftBtn.addEventListener("click", lastSlide);
 //     e.target.classList.add("selector-active");
 //   }
 // });
+const nav = document.querySelector(".navbar");
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  console.log(currentScroll);
+  if (currentScroll <= 0) {
+    nav.classList.remove("scroll-up");
+  }
+  if (currentScroll > lastScroll && !nav.classList.contains("scroll-down")) {
+    nav.classList.remove("scroll-up");
+    nav.classList.add("scroll-down");
+  }
+  if (currentScroll < lastScroll && !nav.classList.contains("scroll-up")) {
+    nav.classList.remove("scroll-down");
+    nav.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
+});
