72272b870c74ac02b7746e141092ba1f4ae2bb3c
WarriorX623 <owenhwtam@gmail.com>
2021-10-10 21:26:59 +0800

    patch?:

--- script.js
+++ script.js
@@ -101,15 +101,16 @@ let lastScroll = 0;
 window.addEventListener("scroll", () => {
   currentScroll = window.pageYOffset;
   console.log(currentScroll);
-  if (currentScroll <= 0) {
-    nav.classList.add("scroll-up");
-  }
+
   if (currentScroll > lastScroll && !nav.classList.contains("scroll-down")) {
     nav.classList.remove("scroll-up");
     nav.classList.add("scroll-down");
   }
   if (currentScroll < lastScroll && !nav.classList.contains("scroll-up")) {
     nav.classList.remove("scroll-down");
+    nav.classList.add("scroll-up");
+  }
+  if (currentScroll <= 0) {
     nav.classList.add("scroll-up");
   }
   lastScroll = currentScroll;
